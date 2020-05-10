9aa5249cb0346136dfea207df7e7ddca972e376a
Francesco Areoluci <francesco.areoluci@magentalab.it>
2020-05-10 20:19:12 +0200

    Removed code splitting, as it is not working in both deployed and local build :(

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -7,10 +7,9 @@ import {
 import ScrollToTop from "./scroll_to_top";
 
 import Home from "./home"
-//import DeviceInfo from "./device_info"
+import DeviceInfo from "./device_info"
 
-const DeviceInfo = React.lazy(() => import("./device_info"));
-
+//const DeviceInfo = React.lazy(() => import("./device_info"));
 
 export default function App() {
   return (
@@ -18,11 +17,12 @@ export default function App() {
       <ScrollToTop>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/deviceInfo" component={DeviceInfo} />
+          {/*
           <Suspense fallback={<div className="lds-ring-wrapper">
                                             <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
                                         </div>}>
-            <Route exact path="/deviceInfo" component={DeviceInfo} />
-          </Suspense>
+          </Suspense>*/}
         </Switch>
       </ScrollToTop>
     </Router>
